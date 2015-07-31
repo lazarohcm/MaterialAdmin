001c2597834cc70fb432df8ea87195b78fb477b6
lazaro.marques <lazarohcm@gmail.com>
2015-07-31 10:29:37 -0300

    Morris Charts Page

---
+++ assets/morrisjs/morris-data.js
@@ -0,0 +1,168 @@
+$(function() {
+    // data stolen from http://howmanyleft.co.uk/vehicle/jaguar_'e'_type
+    var tax_data = [{
+        "period": "2011 Q3",
+        "licensed": 3407,
+        "sorned": 660
+    }, {
+        "period": "2011 Q2",
+        "licensed": 3351,
+        "sorned": 629
+    }, {
+        "period": "2011 Q1",
+        "licensed": 3269,
+        "sorned": 618
+    }, {
+        "period": "2010 Q4",
+        "licensed": 3246,
+        "sorned": 661
+    }, {
+        "period": "2009 Q4",
+        "licensed": 3171,
+        "sorned": 676
+    }, {
+        "period": "2008 Q4",
+        "licensed": 3155,
+        "sorned": 681
+    }, {
+        "period": "2007 Q4",
+        "licensed": 3226,
+        "sorned": 620
+    }, {
+        "period": "2006 Q4",
+        "licensed": 3245,
+        "sorned": null
+    }, {
+        "period": "2005 Q4",
+        "licensed": 3289,
+        "sorned": null
+    }];
+
+    // Line Chart
+    Morris.Line({
+        element: 'morris-line-chart',
+        data: tax_data,
+        xkey: 'period',
+        ykeys: ['licensed', 'sorned'],
+        labels: ['Licensed', 'Off the road'],
+        lineColors:["#0d47a1", "#e65100"]
+    });
+
+    Morris.Area({
+        element: 'morris-area-chart',
+        data: [{
+            period: '2010 Q1',
+            iphone: 2666,
+            ipad: null,
+            itouch: 2647
+        }, {
+            period: '2010 Q2',
+            iphone: 2778,
+            ipad: 2294,
+            itouch: 2441
+        }, {
+            period: '2010 Q3',
+            iphone: 4912,
+            ipad: 1969,
+            itouch: 2501
+        }, {
+            period: '2010 Q4',
+            iphone: 3767,
+            ipad: 3597,
+            itouch: 5689
+        }, {
+            period: '2011 Q1',
+            iphone: 6810,
+            ipad: 1914,
+            itouch: 2293
+        }, {
+            period: '2011 Q2',
+            iphone: 5670,
+            ipad: 4293,
+            itouch: 1881
+        }, {
+            period: '2011 Q3',
+            iphone: 4820,
+            ipad: 3795,
+            itouch: 1588
+        }, {
+            period: '2011 Q4',
+            iphone: 15073,
+            ipad: 5967,
+            itouch: 5175
+        }, {
+            period: '2012 Q1',
+            iphone: 10687,
+            ipad: 4460,
+            itouch: 2028
+        }, {
+            period: '2012 Q2',
+            iphone: 8432,
+            ipad: 5713,
+            itouch: 1791
+        }],
+        xkey: 'period',
+        ykeys: ['iphone', 'ipad', 'itouch'],
+        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true,
+        lineColors: ["#00c853", "#263238", "#0091ea"]
+    });
+
+    Morris.Donut({
+        element: 'morris-donut-chart',
+        data: [{
+            label: "Download Sales",
+            value: 12
+        }, {
+            label: "In-Store Sales",
+            value: 30
+        }, {
+            label: "Mail-Order Sales",
+            value: 20
+        }],
+        resize: true,
+        colors: ["#3d5afe", "#00bfa5", "#f50057"]
+    });
+
+    Morris.Bar({
+        element: 'morris-bar-chart',
+        data: [{
+            y: '2006',
+            a: 100,
+            b: 90
+        }, {
+            y: '2007',
+            a: 75,
+            b: 65
+        }, {
+            y: '2008',
+            a: 50,
+            b: 40
+        }, {
+            y: '2009',
+            a: 75,
+            b: 65
+        }, {
+            y: '2010',
+            a: 50,
+            b: 40
+        }, {
+            y: '2011',
+            a: 75,
+            b: 65
+        }, {
+            y: '2012',
+            a: 100,
+            b: 90
+        }],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B'],
+        hideHover: 'auto',
+        resize: true,
+        barColors: ["#00b8d4", "#d50000"]
+    });
+
+});
